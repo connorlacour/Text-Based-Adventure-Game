13d866c38199e368f08d6ef75006121302a87073
Caroline <55813961+LadyCare@users.noreply.github.com>
2021-08-07 04:16:46 -0500

    synonym list and utf-8 cleanup
    created synonym lists and cleaned up apostrophes and ellipses causing utf-8 printout issues

--- game_objects/synonyms.py
+++ game_objects/synonyms.py
@@ -8,20 +8,85 @@ look_synonyms = {
 }
 
 take_synonyms = {
-    "TAKE", "GRAB", "PICK UP"
+    "TAKE", "GRAB", "PICK UP", "CARRY"
 }
 
 drop_synonyms = {
     "DROP", "PUT DOWN", "PLACE DOWN", "DISCARD", "CHUCK", "THROW OUT", "THROW AWAY"
 }
 
+wear_synonyms = {
+    "WEAR", "PUT ON", "EQUIP"
+}
+
+press_synonyms = {
+    "PRESS", "TOUCH", "MASH"
+}
+
+fight_synonyms = {
+    "FIGHT", "FIGHT WITH", "ATTACK", "BATTLE", "CHALLENGE", "RESIST", "BRAWL", "DUEL", "FEUD", "QUARREL", "SPAR", "STRUGGLE", "WRESTLE", "BOX"
+}
+
+eat_synonyms = {
+    "EAT", "BITE", "CHEW", "CONSUME", "DEVOUR", "DINE", "GOBBLE", "NIBBLE", "GRAZE", "MUNCH", "SCARF", "SNACK"
+}
+
+turn_on_synonyms = {
+    "TURN ON", "ACTIVATE", "SWITCH ON", "START UP"
+}
+
+swallow_synonyms = {
+    "SWALLOW", "CONSUME", "GULP", "IMBIBE", "INGEST"
+}
+
+unlock_synonyms = {
+    "UNLOCK", "UNLOCK WITH", "OPEN WITH"    
+}
+
+use_synonyms = {
+    "USE", "APPLY", "EMPLOY", "OPERATE", "UTILIZE", "WEILD"
+}
+
+bribe_synonyms = {
+    "BRIBE", "BUY OFF", "ENTICE", "PAY OFF", "TEMPT"
+}
+
+persuade_synonyms = {
+    "PERSUADE", "COAX", "SWAY", "TALK INTO", "URGE", "WIN OVER"
+}
+
+enter_code_synonyms = {
+    "ENTER", "ENTER CODE", "INSERT", "PUT IN"
+}
+
+feed_synonyms = {
+    "GIVE", "NOURISH", "PROVIDE", "SATISFY", "STUFF", "SUPPLY", "HAND"
+}
+
+open_synonyms = {
+    "CRACK", "CRACK OPEN", "UNLATCH" "UNFASTEN", 
+}
+
+pick_up_synonyms = {
+    "PULL", "LIFT", "RAISE"
+}
 
 synonym_dict = {
     "GO": list(go_synonyms),
     "LOOK": list(look_synonyms),
     "DROP": list(drop_synonyms),
-    "WASH": ["SCRUB", "CLEAN"],
-    "COMBINE": ["USE", "SMASH TOGETHER", "PUT IN"],
-    "PLACE": ["PUT DOWN", "PUT ON"],
-    "BARF": ["THROW UP", "WRETCH", "SPILL GUTS", "VOMIT"]
+    "BRIBE": list(bribe_synonyms),
+    "EAT": list(eat_synonyms),
+    "ENTER CODE": list(enter_code_synonyms),
+    "FEED": list(feed_synonyms),
+    "FIGHT": list(fight_synonyms),
+    "OPEN": list(open_synonyms),
+    "PERSUADE": list(persuade_synonyms),
+    "PICK UP": list(pick_up_synonyms),
+    "PRESS": list(press_synonyms),
+    "SWALLOW": list(swallow_synonyms),
+    "TURN ON": list(turn_on_synonyms),
+    "UNLOCK": list(unlock_synonyms),
+    "USE": list(use_synonyms),
+    "WEAR": list(wear_synonyms)
 }
