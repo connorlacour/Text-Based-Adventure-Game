2409e1991e219514b8e04f07f4708302ad03ed30
Emily.Sorg <emily.sorg@bestbuy.com>
2021-08-08 13:20:25 -0700

    Fix multi word verbs not being added to action list

--- parse_cmd.py
+++ parse_cmd.py
@@ -29,11 +29,20 @@ actions = {'GO',
 
 
 def setup_parser():
-    from game_objects.global_collections import rooms
+    from game_objects.global_collections import rooms, items
     #Add all exisiting directions to dirs
     for room in rooms.values():
         for direction in room.connecting_rooms.keys():
             directions.add(direction.upper())
+
+    for item in items.values():
+        for e in item.events.values():
+            if len(e.verb.split(sep=" ")) > 1:
+                actions.add(e.verb)
+            for syn in e.synonyms:
+                if len(syn.split(sep=" ")) > 1:
+                    actions.add(syn)
+
 
 
 
